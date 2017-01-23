4ad5afd2a21bb10d13f804dea4167a13858787f8
ralph <mail@mail.mail>
2017-01-24 01:53:39 +0300

    added telegram to controller (cherry picked from commit 4a33784)

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -2,6 +2,8 @@ class RequestsController < ApplicationController
   def create
     Rails.logger.info "zayavka: #{params.inspect}"
 
+    TelegramSender.send_message "Поступила заявка:\n#{params.inspect}"
+
     redirect_to '/zayavka_poluchena'
   end
 end
