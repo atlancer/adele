4a3378473b3f3df47747029648e1752286dea141
ralph <mail@mail.mail>
2017-01-24 01:53:39 +0300

    added telegram to controller

--- config/deploy.rb
+++ config/deploy.rb
@@ -34,7 +34,7 @@ set :passenger_restart_with_touch, true
 # set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
 
 # Default value for :linked_files is []
-set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
+set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml', 'config/telegram_chats.yml', 'telegram_secrets.yml')
 
 # Default value for linked_dirs is []
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle')
