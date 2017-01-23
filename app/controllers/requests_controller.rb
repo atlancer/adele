0c4b218dbb6f153a2aa45a35697bba599130c543
ralph <mail@mail.mail>
2017-01-24 02:08:17 +0300

    updated telegram message

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -2,7 +2,7 @@ class RequestsController < ApplicationController
   def create
     Rails.logger.info "zayavka: #{params.inspect}"
 
-    TelegramSender.send_message "Поступила заявка:\n#{params.inspect}"
+    TelegramSender.send_message "Поступила заявка на обучение.\nИмя: #{params['name']}\nEmail: #{params['email']}\nТелефон: #{params['phone']}"
 
     redirect_to '/zayavka_poluchena'
   end
