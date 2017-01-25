ffca309942b9d72c759e80425992029872755cb7
ralph <mail@mail.mail>
2017-01-25 18:50:08 +0300

    fixed TelegramSender

--- lib/telegram_sender.rb
+++ lib/telegram_sender.rb
@@ -1,17 +1,18 @@
 require 'telegram/bot'
 
 class TelegramSender
-  CHATS = YAML.load_file(Rails.root.join('config', 'telegram_chats.yml')) rescue []
   SECRETS = YAML.load_file Rails.root.join('config', 'telegram_secrets.yml')
   TELEGRAM_LOGGER = Logger.new Rails.root.join('log', 'telegram.log')
 
   def self.send_message(message)
+    chats = YAML.load_file(Rails.root.join('config', 'telegram_chats.yml')) rescue []
+
     Telegram::Bot::Client.run(SECRETS['token']) do |bot|
-      CHATS.each do |chat_id|
+      chats.each do |chat_id|
         bot.api.sendMessage(chat_id: chat_id, text: message)
       end
     end
 
-    TELEGRAM_LOGGER.info "Send: #{CHATS.inspect}, Message: #{message}"
+    TELEGRAM_LOGGER.info "Send: #{chats.inspect}, Message: #{message}"
   end
 end
